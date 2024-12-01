7a4ee0f0c2c17ea299eabb159ef7702c71300702
krasiren <64616509+krasiren@users.noreply.github.com>
2024-12-01 19:15:26 +0100

    day 1 completed

--- 2024/day 01/part1.py
+++ 2024/day 01/part1.py
@@ -1,2 +1,18 @@
 with open('2024/day 01/input.txt', 'r') as f:
 	lines = f.readlines()
+
+first = []
+second = []
+for line in lines:
+	a, b = int(line.split('   ')[0]), int(line.split('   ')[1])
+	first.append(a)
+	second.append(b)
+
+first.sort()
+second.sort()
+
+sum = 0
+for i in range(len(first)):
+	sum += abs(first[i] - second[i])
+
+print(sum)
--- 2024/day 01/part2.py
+++ 2024/day 01/part2.py
@@ -1,2 +1,25 @@
 with open('2024/day 01/input.txt', 'r') as f:
 	lines = f.readlines()
+
+left = []
+right = []
+for l in lines:
+	nums = l.split('   ')
+	left.append(int(nums[0]))
+	right.append(int(nums[1]))
+
+# how many times a number appears in THEIR OWN list
+right_count = {}
+for n in right:
+	if n in right_count:
+		right_count[n] += 1
+	else:
+		right_count[n] = 1
+
+# finally get the similarity
+similarty = 0
+for n in left:
+	if n in right_count:
+		similarty += n * right_count[n]
+
+print(similarty)
