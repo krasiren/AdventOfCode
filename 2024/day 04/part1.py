e32999dc3fa8773afba1688bccff11c965b11ee3
krasiren <64616509+krasiren@users.noreply.github.com>
2024-12-04 21:29:54 +0100

    day 4 complete

--- 2024/day 04/part1.py
+++ 2024/day 04/part1.py
@@ -1,2 +1,41 @@
 with open('2024/day 04/input.txt', 'r') as f:
-	lines = f.readlines()
+	lines = f.read().split('\n')
+	
+# These two may not be needed
+def is_legit(num):
+	if num < 0 or num >= len(lines):
+		raise Exception('Wrong index')
+	return num
+
+def find_words(start_i, start_j, dir_i=None, dir_j=None, char_ix=0):
+	# if we found all letters in the `word` increase counter (return 1)
+	if char_ix == 4:
+		return 1
+	
+	# The first time we come in this function (form double for loop),
+	# return the sum of all directions
+	if dir_i is None and dir_j is None:
+		return sum([find_words(start_i, start_j, ver, hor, char_ix+1) 
+			  for hor in {-1, 0, 1} for ver in {-1, 0, 1}])
+	
+	# A condition to check for valid indeces
+	try:
+		cur_i = is_legit(start_i + dir_i)
+		cur_j = is_legit(start_j + dir_j)
+	except:
+		return 0
+	
+	if lines[cur_i][cur_j] == word[char_ix]:
+		return find_words(cur_i, cur_j, dir_i, dir_j, char_ix+1)
+	return 0
+
+
+# Go through all characters and for every `X` search in all directions
+word = ['X', 'M', 'A', 'S']
+counter = 0
+for i, row in enumerate(lines):
+	for j, char in enumerate(row):
+		if char == 'X':
+			counter += find_words(i, j)
+
+print(counter)
--- 2024/day 04/part2.py
+++ 2024/day 04/part2.py
@@ -1,2 +1,33 @@
 with open('2024/day 04/input.txt', 'r') as f:
-	lines = f.readlines()
+	lines = f.read().split('\n')
+
+def find_x_mases(x, y, direction=None):
+	def is_valid_x_mas():
+		remaining = ['M', 'M', 'S', 'S']
+		for k in range(1, 4):
+			next_c = corners[(i+k) % 4]
+			if lines[x+next_c[0]][y+next_c[1]] != remaining[k]:
+				return False
+		return True
+	
+	for i, corner in enumerate(corners):
+		if lines[x+corner[0]][y+corner[1]] == 'M':
+			if is_valid_x_mas():
+				return 1
+	
+	return 0
+
+
+# Go through all characters and for every `A` search for `MAS`
+# Can skip outer walls, cuz `A` is in the center
+# These must be cyclic (curently counter clock-wise)
+corners = [(-1, -1), (-1, 1), (1, 1), (1, -1)]
+word = ['X', 'M', 'A', 'S']
+counter = 0
+for i, row in enumerate(lines):
+	if i not in {0, len(lines)-1}:
+		for j, char in enumerate(row):
+			if j not in {0, len(lines[0])-1} and char == 'A':
+				counter += find_x_mases(i, j)
+
+print(counter)
