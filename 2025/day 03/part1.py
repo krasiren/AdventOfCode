a3e0f39d554e77533cb929979b9f5eb9b3765d1b
krasiren <tdjkraevec55@gmail.com>
2025-12-03 18:16:55 +0100

    day3 done

--- 2025/day 03/part1.py
+++ 2025/day 03/part1.py
@@ -1,2 +1,24 @@
-with open('2025/day 03/input.txt', 'r') as f:
-	lines = f.readlines()
+def bank_joltage(bank: str) -> int:
+    tenths, tenths_ix = 0, 0
+    for i, c in enumerate(bank[:-1]):
+        c = int(c)
+        if c > tenths:
+            tenths = c
+            tenths_ix = i
+        if c == 9:
+            break
+
+    ones = 0
+    for c in bank[tenths_ix + 1:]:
+        c = int(c)
+        if c > ones:
+            ones = c
+
+    return int(f'{tenths}{ones}')
+
+
+if __name__ == '__main__':
+    with open('2025/day 03/input.txt', 'r') as f:
+        lines = f.read().splitlines()
+
+    print(sum(bank_joltage(line) for line in lines))
--- 2025/day 03/part2.py
+++ 2025/day 03/part2.py
@@ -1,2 +1,24 @@
-with open('2025/day 03/input.txt', 'r') as f:
-	lines = f.readlines()
+from pathlib import Path
+
+
+def bank_joltage(bank: str, start_ix=0, sub_bank=11) -> str:
+    if sub_bank < 0:
+        return ''
+
+    place, place_ix = 0, 0
+    for i, c in enumerate(bank[start_ix:len(bank)-sub_bank]):
+        c = int(c)
+        if c > place:
+            place = c
+            place_ix = i + start_ix
+        if c == 9:
+            break
+
+    return str(place) + bank_joltage(bank, place_ix + 1, sub_bank - 1)
+
+
+if __name__ == '__main__':
+    with (Path(__file__).parent / 'input.txt').open('r') as f:
+        lines = f.read().splitlines()
+
+    print(sum(int(bank_joltage(line)) for line in lines))
