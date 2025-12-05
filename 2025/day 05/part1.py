a05335cbecbdb5b7ed0a4a9e657954b446700990
krasiren <tdjkraevec55@gmail.com>
2025-12-05 15:01:59 +0100

    day5, part1 working, part2 needs some work

--- 2025/day 05/part1.py
+++ 2025/day 05/part1.py
@@ -1,2 +1,20 @@
 with open('2025/day 05/input.txt', 'r') as f:
-	lines = f.readlines()
+    lines = f.read().splitlines()
+
+line_split_ix = lines.index('')
+
+ranges = []
+for line in lines[:line_split_ix]:
+    start, end = line.split('-')
+    ranges.append((int(start), int(end)))
+
+
+num_fresh = 0
+for line in lines[line_split_ix + 1:]:
+    num = int(line)
+    for start, end in ranges:
+        if start <= num <= end:
+            num_fresh += 1
+            break
+
+print(num_fresh)
--- 2025/day 05/part2.py
+++ 2025/day 05/part2.py
@@ -1,2 +1,43 @@
 with open('2025/day 05/input.txt', 'r') as f:
-	lines = f.readlines()
+    lines = f.read().splitlines()
+
+line_split_ix = lines.index('')
+
+ranges = []
+for line in lines[:line_split_ix]:
+    start, end = line.split('-')
+    ranges.append((int(start), int(end)))
+
+
+def join_ranges(all_ranges):
+    merged = []
+    for i, r in enumerate(all_ranges):
+        f_start, f_end = r
+        was_merged = False
+        for j, rs in enumerate(all_ranges):
+            s_start, s_end = rs
+            if i == j:
+                continue
+            if (f_start <= s_start <= f_end) or (f_start <= s_end <= f_end):
+                merged.append((min(f_start, s_start), max(f_end, s_end)))
+                was_merged = True
+
+        if not was_merged:
+            merged.append(r)
+
+    return list(set(merged))
+
+
+while True:
+    new_ranges = join_ranges(ranges)
+    if len(new_ranges) == len(ranges):
+        break
+    ranges = new_ranges
+
+
+num_ids = 0
+for start, end in ranges:
+    num_ids += end - start + 1
+
+print(num_ids)
+
