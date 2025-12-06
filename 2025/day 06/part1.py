1a671d1f56b5930adcf5ec6110f669cce4aebecd
krasiren <tdjkraevec55@gmail.com>
2025-12-06 12:37:11 +0100

    day6 done

--- 2025/day 06/part1.py
+++ 2025/day 06/part1.py
@@ -1,2 +1,24 @@
+from operator import add, mul
+from functools import reduce
+
+
 with open('2025/day 06/input.txt', 'r') as f:
-	lines = f.readlines()
+    lines = f.read().splitlines()
+
+
+problems = []
+problem_operations = []
+for i, line in enumerate(lines):
+    problem_ix = 0
+    for el in line.split(' '):
+        if not el:
+            continue
+        if i == 0:
+            problems.append([int(el)])
+        elif i == len(lines)-1:
+            problem_operations.append(el)
+        else:
+            problems[problem_ix].append(int(el))
+        problem_ix += 1
+
+print(sum(reduce(add if problem_operations[i] == '+' else mul, problems[i]) for i in range(len(problems))))
--- 2025/day 06/part2.py
+++ 2025/day 06/part2.py
@@ -1,2 +1,25 @@
-with open('2025/day 06/input.txt', 'r') as f:
-	lines = f.readlines()
+from operator import add, mul
+from functools import reduce
+from pathlib import Path
+
+
+with (Path(__file__).parent / 'input.txt').open() as f:
+    lines = f.read().splitlines()
+
+# Manually add spaces to make all lines the same length
+assert all(len(l) == len(lines[0]) for l in lines)
+
+problems = []
+problem_operations = []
+for j in range(len(lines[0])):
+    if lines[-1][j] in ('+', '*'):
+        problem_operations.append(lines[-1][j])
+        problems.append([])
+    column_num = ''
+    for i in range(len(lines)-1):
+        column_num += lines[i][j].strip()
+    if column_num:
+        problems[-1].append(int(column_num))
+
+
+print(sum(reduce(add if problem_operations[i] == '+' else mul, problems[i]) for i in range(len(problems))))
