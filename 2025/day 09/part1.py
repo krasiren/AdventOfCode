a92fcee122ef7315091ef37e79bc82c0570c7e78
krasiren <tdjkraevec55@gmail.com>
2025-12-09 18:30:48 +0100

    day9 done - very cute topological one

--- 2025/day 09/part1.py
+++ 2025/day 09/part1.py
@@ -1,2 +1,16 @@
-with open('2025/day 09/input.txt', 'r') as f:
-	lines = f.readlines()
+if __name__ == "__main__":
+    with open('2025/day 09/input.txt', 'r') as f:
+        lines = f.read().splitlines()
+
+    points = [tuple(map(int, line.split(','))) for line in lines]
+
+    max_area = 0
+    for p1 in points:
+        for p2 in points:
+            if p1 == p2:
+                continue
+            area = (abs(p1[0] - p2[0])+1) * (abs(p1[1] - p2[1])+1)
+            if area > max_area:
+                max_area = area
+
+    print(max_area)
--- 2025/day 09/part2.py
+++ 2025/day 09/part2.py
@@ -1,2 +1,25 @@
-with open('2025/day 09/input.txt', 'r') as f:
-	lines = f.readlines()
+from shapely import LineString
+from shapely.geometry.polygon import Polygon
+
+if __name__ == "__main__":
+    with open('2025/day 09/input.txt', 'r') as f:
+        lines = f.read().splitlines()
+
+    points = [tuple(map(int, line.split(','))) for line in lines]
+    poly = Polygon(points)
+
+    max_area = 0
+    for p1 in points:
+        for p2 in points:
+            if any(p1[i] == p2[i] for i in (0, 1)):
+                shape = LineString([p1, p2])
+            else:
+                shape = Polygon([p1, (p1[0], p2[1]), p2, (p2[0], p1[1])])
+
+            if shape.covered_by(poly):
+                area = (abs(p1[0] - p2[0])+1) * (abs(p1[1] - p2[1])+1)
+                if area > max_area:
+                    max_area = area
+                    print(p1, p2, area)
+
+    print(max_area)
