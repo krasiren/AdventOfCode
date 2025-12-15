cd09452d6d1a986b31d5016646f0438d810d0afa
krasiren <tdjkraevec55@gmail.com>
2025-12-15 18:58:48 +0100

    day10 part2 - kys

--- 2025/day 10/part2.py
+++ 2025/day 10/part2.py
@@ -1,36 +1,43 @@
 import re
+from functools import lru_cache
 from pathlib import Path
+
+import numpy as np
 
 
 def to_joltages(s):
     return [int(j) for j in s.split(',')]
+    return np.array([int(j) for j in s.split(',')]).reshape(1, -1)
 
 
-def to_buttons(lst):
-    return [[int(i) for i in b.split(',')] for b in lst]
+def to_vectors(lst, size: int):
+    vecs = [[1 if i in map(int, b.split(',')) else 0 for i in range(size)] for b in lst]
+    return vecs
+    return np.array(vecs)
 
 
 def parse_line_content(s):
     pattern = r'\[(.*?)\]|\((.*?)\)|\{(.*?)\}'
     matches = re.findall(pattern, s)
     matches = [it for m in matches for it in m if it]
-    return matches[0], to_buttons(matches[1:-1]), to_joltages(matches[-1])
+    return matches[0], to_vectors(matches[1:-1], len(matches[0])), to_joltages(matches[-1])
 
 
-def get_min_steps(end, btns: list[list[int]]) -> int:
-    steps = 0
-    previous_states = [[0] * len(end)]
-    while True:
-        steps += 1
-        next_states = []
-        for s in previous_states:
-            for b in btns:
-                next_state = [s[i] + (i in b) for i in range(len(s))]
-                if all(next_state[i] == end[i] for i in range(len(end))):
-                    return steps
-                next_states.append(next_state)
-
-        previous_states = next_states
+def get_min_steps(end, vecs) -> int:
+    @lru_cache
+    def solve(current) -> int | float:
+        if all(current[i] == end[i] for i in range(len(end))):
+            return 0
+        if any(current[i] > end[i] for i in range(len(end))):
+            return float('inf')
+        min_steps = float('inf')
+        for v in vecs:
+            steps = solve(tuple(current[i] + v[i] for i in range(len(end))))
+            if steps < min_steps:
+                min_steps = steps
+        return min_steps + 1
+    solve.cache_info()
+    return int(solve(tuple([0] * len(end))))
 
 
 if __name__ == "__main__":
@@ -38,9 +45,10 @@ if __name__ == "__main__":
         lines = f.read().splitlines()
 
     total = 0
-    for line in lines:
+    for i, line in enumerate(lines):
         _, buttons, joltage_levels = parse_line_content(line)
 
-        total += get_min_steps(joltage_levels, buttons)
-
+        print(i)
+        t = get_min_steps(joltage_levels, buttons)
+        total += t
     print(total)
